dca1d00d6f4fdc4d0b52bfda8165eb4df796c4c1
ezkl123 <hammadkhattak180903@gmail.com>
2024-09-03 13:49:33 +0100

    added onclick event listeners to github and linkedin buttons, added my projects so far

---
+++ index.js
@@ -0,0 +1,15 @@
+const LinkedIn = document.getElementById("linkedin-button")
+
+LinkedIn.addEventListener("click", handleLinkedInClick)
+
+function handleLinkedInClick(event){
+    window.open("https://www.linkedin.com/in/muhammad-hammad-5534ba258/", "_blank")
+}
+
+const Github = document.getElementById("github-button")
+
+Github.addEventListener("click", handleGithubClick)
+
+function handleGithubClick(event){
+    window.open("https://github.com/ezkl123", "_blank")
+}

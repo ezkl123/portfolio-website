f35f0f4f5b6f3ef214a473215920138f272947da
ezkl123 <hammadkhattak180903@gmail.com>
2025-04-19 23:39:10 +0100

    removed gaps between nav links, added display and padding to nav links to give them each a visible box around the writing. added smooth scrolling redirection from the nav links to the relevant parts of the page. finally added hover functionality to the navbar where the text changes to white and the background colour of the navlink changes brown

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
+const navbarHover = document.querySelectorAll('.nav-styling')
+
+navbarHover.forEach((element) => {
+    element.addEventListener("mouseenter", () => {
+        element.style.backgroundColor = "rgb(211, 123, 80)";
+        element.style.color = "rgb(255, 255, 255)"
+
+    })
+
+    element.addEventListener("mouseleave", () => {
+        element.style.backgroundColor = "rgb(245, 245, 220)"
+        element.style.color = "rgb(0, 0, 0)"
+    })
+})
+
+
+
 const LinkedIn = document.getElementById("linkedin-button")
 
-LinkedIn.addEventListener("click", handleLinkedInClick)
+// LinkedIn.addEventListener("click", handleLinkedInClick)
 
-function handleLinkedInClick(event){
-    window.open("https://www.linkedin.com/in/muhammad-hammad-5534ba258/", "_blank")
-}
+// function handleLinkedInClick(event){
+//     window.open("https://www.linkedin.com/in/muhammad-hammad-5534ba258/", "_blank")
+// }
 
-const Github = document.getElementById("github-button")
+// const Github = document.getElementById("github-button")
 
-Github.addEventListener("click", handleGithubClick)
+// Github.addEventListener("click", handleGithubClick)
 
-function handleGithubClick(event){
-    window.open("https://github.com/ezkl123", "_blank")
-}
+// function handleGithubClick(event){
+//     window.open("https://github.com/ezkl123", "_blank")
+// }
